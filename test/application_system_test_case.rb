568ef6d3e6e9a5197df749a7cb7d4816af5b5e9c
Ramon Ferreira Silva <ramon.silva@rdstation.com>
2023-05-14 16:39:36 -0300

    Add Login and Auth

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Warden::Test::Helpers
+
   driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
 
   def take_failed_screenshot
